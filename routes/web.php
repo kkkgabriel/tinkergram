4a30823ecbba8b3f4b32f8c36fd1bfffdeb1fc6a
Raphael Koh <raphael@Raphaels-Mac-mini.local>
2021-09-07 18:11:39 +0800

    changed some path names, no effect on functionality

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
 Route::get('/profile/create', [App\Http\Controllers\ProfileController::class, 'create']);
-Route::post('/profile', [App\Http\Controllers\ProfileController::class, 'postCreate'])->name('profile.postCreate');
+Route::post('/profile/postCreate', [App\Http\Controllers\ProfileController::class, 'postCreate'])->name('profile.postCreate');
 Route::get('/profile/edit', [App\Http\Controllers\ProfileController::class, 'edit']);
-Route::post('/profile/{id}/update', [App\Http\Controllers\ProfileController::class, 'postEdit'])->name('profile.postEdit');
+Route::post('/profile/{id}/postEdit', [App\Http\Controllers\ProfileController::class, 'postEdit'])->name('profile.postEdit');
 
 Route::resource('post', App\Http\Controllers\PostController::class);
